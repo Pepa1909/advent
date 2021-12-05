5b88900bf7c09086225d8fc23872a74843fd7f1f
Josef Vojkovský <pepavoj@email.cz>
2021-12-05 18:54:33 +0100

    1 lepsi

--- 1.py
+++ 1.py
@@ -1,13 +1,11 @@
 import csv
 
-with open("1.csv", encoding="utf-8") as csvfile:
-    reader = csv.reader(csvfile)
-    
-    list=[]
+with open("1.txt", encoding="utf-8") as f:
     vyssi = 0
-    for row in reader:
-        list.append(int(row[0]))
-        if len(list)>1:
-            if list[-1] > list[-2]:
-                vyssi += 1
+    l =[]
+    for r in f:
+        l.append(int(r))
+        if len(l) > 1:
+            if l[-1] > l[-2]:
+                vyssi+=1
     print(vyssi)
